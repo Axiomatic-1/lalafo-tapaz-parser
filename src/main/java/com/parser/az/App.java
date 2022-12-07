77b176d8c5ca3cb3e1cd85dd8d9ee9a01362967b
nazir.ismailov <gympills@gmail.com>
2022-12-07 15:24:38 +0500

    added new filters

--- src/main/java/com/parser/az/App.java
+++ src/main/java/com/parser/az/App.java
@@ -10,12 +10,12 @@ import java.util.Timer;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        System.out.println("For beginning write what you want to search here is 3 options:" +
+        System.out.println(" For beginning write what you want to search here is 3 options:" +
                 " \n cpu \n videocard \n motherboard");
         Timer time = new Timer(); // Instantiate Timer Object
         SchedulerService st = new SchedulerService(); // Instantiate SheduledTask class
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         SchedulerService.productType = reader.readLine();
-        time.schedule(st, 0, 2000);
+        time.schedule(st, 0, 10000);
     }
 }
